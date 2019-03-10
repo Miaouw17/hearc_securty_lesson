1c757a222bc936e811fbcb911db442e9d471724d
Fabien Mottier <fabien.mottier@he-arc.ch>
2019-03-10 13:16:27 +0100

    base client-server

---
+++ ChallengeResponse/ChallengeResponse/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeResponse
+{
+    class Client
+    {
+        private RSACryptoServiceProvider rsa;
+        public Client()
+        {
+            rsa = new RSACryptoServiceProvider();
+        }
+
+        public byte[] RSAEncrypt(byte[] DataToEncrypt, bool DoOAEPPadding)
+        {
+            try
+            {
+                //Import the RSA Key information. This only needs
+                //toinclude the public key information.
+                rsa.ImportParameters(Server.Instance.getRSAParameters());
+
+                //Encrypt the passed byte array and specify OAEP padding.  
+                //OAEP padding is only available on Microsoft Windows XP or
+                //later.  
+                byte[]  encryptedData = rsa.Encrypt(DataToEncrypt, Server.Instance.OAEPPadding);
+
+                return encryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+
+        }
+    }
+}
---
+++ ChallengeResponse/ChallengeResponse/Server.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Security.Cryptography;
+
+namespace ChallengeResponse
+{
+    public class Server
+    {
+        private static Server instance;
+        public static Server Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Server();
+                }
+                return instance;
+            }
+        }
+
+        private RSACryptoServiceProvider rsa;
+        public RSAEncryptionPadding OAEPPadding { get; }
+
+        private Server()
+        {
+            rsa = new RSACryptoServiceProvider();
+            OAEPPadding = RSAEncryptionPadding.OaepSHA512;
+        }
+
+        public RSAParameters getRSAParameters()
+        {
+            return rsa.ExportParameters(false);
+        }
+
+        public byte[] RSADecrypt(byte[] DataToDecrypt, bool DoOAEPPadding)
+        {
+            try
+            {
+                //Decrypt the passed byte array and specify OAEP padding.  
+                //OAEP padding is only available on Microsoft Windows XP or
+                //later. 
+                byte[] decryptedData = rsa.Decrypt(DataToDecrypt, OAEPPadding);
+                return decryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+
+        }
+    }
+}
